c43170d1b91a8c34108588afd3ca23758e5b0cfb
Chang Minwook <fromme0528@gmail.com>
2018-07-10 14:38:19 +0900

    Update data loader and split

---
+++ data_split.py
@@ -0,0 +1,55 @@
+#data_split.py
+
+import os
+import librosa
+
+path_dir = "./data/"
+output_dir = "./data_split/"
+
+file_list = os.listdir(path_dir) 
+
+wav_files = [f for f in file_list if f[-12:] == "denoised.wav"]
+txt_files = [f for f in file_list if f[-4:] == ".txt"]
+
+wav_files.sort()
+txt_files.sort()
+
+print("wav files")
+print(wav_files)
+
+print("txt files")
+print(txt_files)
+
+data_list_1 = []
+data_list_2 = []
+data_list_3 = []
+
+for path in txt_files:
+    print(path)
+    f = open(path_dir+path,'r')
+    data =f.read()
+    
+    data_list = data.split("\n")
+    data_list = data_list [:-1]
+    data_list = [data for data in data_list if data.find("None",9) == -1]
+    
+    print(data_list)
+    
+    temp_hit = [float(data.split(" ")[0]) for data in data_list]
+    data_list_1.append(temp_hit)
+    data_list_2.append([data.split(" ")[1] for data in data_list])
+    data_list_3.append([data.split(" ")[2] for data in data_list])
+    
+    f.close()
+    
+#for 1 wav file
+for idx1 in range(len(data_list_1)):
+    
+    y, sr = librosa.load(path_dir+wav_files[idx1],sr=16000)
+    
+    for idx2 in range(len(data_list_1[idx1])):
+        # 시간 계산
+        curr_time = data_list_1[idx1][idx2]
+        output_path = output_dir + txt_files[idx1][:-9]+data_list_2[idx1][idx2]+"_"+data_list_3[idx1][idx2]+"_"+str(idx1)+"_"+str(idx2)+".wav"
+        print(output_path)
+        librosa.output.write_wav(output_path, y[int(sr*curr_time):int(sr*curr_time)+16384],sr)
---
+++ dataloader.py
@@ -0,0 +1,35 @@
+from torch.utils.data import DataLoader
+
+
+path_dir = "./data_split/"
+
+
+class AudioLoader(torchData.Dataset):
+    
+    def __init__(self, inPath, isShuffle = False):
+        
+        files = os.listdir(inPath)
+        
+        files = [f for f in files if f[-4:] == ".wav"]
+        
+#        files = [f for f in files if (f.find("wood",18) != -1 or f.find("metal",18) != -1)]
+        files = [f for f in files if (f.find("ceramic",18) != -1 or f.find("metal",18) != -1)]
+        
+        if isShuffle:
+            random.shuffle(files)
+        
+        self.inPath = inPath
+        self.isShuffle = isShuffle
+        self.len = len(files)  
+        self.files = files
+        print("# of wav files : ", self.len)
+    
+    def __getitem__(self, idx):
+        
+        y, sr = librosa.load(self.inPath+self.files[idx], 16000)
+        y = torch.from_numpy(y)
+
+        return y
+    
+    def __len__(self):
+        return self.len
